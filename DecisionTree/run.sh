2a9da15733d158d1868e2eedadeb3fc4392dec05
LSJ <leelasowmya.jandhyala@gmail.com>
2024-09-20 20:04:25 -0600

    adding run.sh file

---
+++ DecisionTree/run.sh
@@ -0,0 +1,7 @@
+# Make sure you are in DecisionTree folder when you execute this
+
+# Run this Python script for Car problem
+python3 car.py
+
+# Run this Python script for Bank problem
+python3 bank.py
